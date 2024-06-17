3bba5de9975552914780b620c0aca93ba520a595
pablo3V <pablo7423@gmail.com>
2024-06-17 17:05:04 +0200

    Update files and add new files

--- dash_app.py
+++ dash_app.py
@@ -439,7 +439,6 @@ default_number_neighbours = 500
 
 # Create a dash application
 app = dash.Dash(__name__, suppress_callback_exceptions=True)
-server = app.server
 
 
 ###############################################################################
@@ -827,10 +826,10 @@ def update_intermediate_state(app_state, rating_store):
         potential_recommendations = knn_model(ratings_csr_matrix, users, target_UserID, default_number_neighbours, selected_ratings, target_books)   
 
         # Recommend just the first volume of a saga or the following to the one the user has read
-        #target_user_books = ratings_new[ratings_new['UserID'] == target_UserID]
-        #target_user_books = pd.merge(target_user_books, books[['BookID', 'Title', 'Saga_Name', 'Saga_Volume']], on='BookID', how='left')
-        #potential_recommendations = pd.merge(potential_recommendations, books[['BookID', 'Title', 'Saga_Name', 'Saga_Volume']], on='BookID', how='left')
-        #potential_recommendations = update_recommendations(potential_recommendations, target_user_books, books)
+        target_user_books = ratings_new[ratings_new['UserID'] == target_UserID]
+        target_user_books = pd.merge(target_user_books, books[['BookID', 'Title', 'Saga_Name', 'Saga_Volume']], on='BookID', how='left')
+        potential_recommendations = pd.merge(potential_recommendations, books[['BookID', 'Title', 'Saga_Name', 'Saga_Volume']], on='BookID', how='left')
+        potential_recommendations = update_recommendations(potential_recommendations, target_user_books, books)
         
         potential_recommendations_json = potential_recommendations.to_json(orient='split')
     
@@ -1158,6 +1157,6 @@ def get_the_final_recommendations(app_state, pot_recom_json, selected_genres, ex
     
 
 if __name__ == '__main__':
-    app.server(host='0.0.0.0', port=8080, debug=True)
+    app.run_server(debug=True)
     
 #######################################################################################################
